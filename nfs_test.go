6f966f650939f9567525b36e76d2a89d2dbf2d7d
Frans Kaashoek <kaashoek@mit.edu>
2019-11-24 16:38:32 -0500

    x

--- nfs_test.go
+++ nfs_test.go
@@ -29,55 +29,53 @@ func (suite *NfsSuite) TestGetRoot() {
 }
 
 func (suite *NfsSuite) Create(name string) {
-	nfs := suite.nfs
 	where := Diropargs3{Dir: MkRootFh3(), Name: Filename3(name)}
 	how := Createhow3{}
 	args := &CREATE3args{Where: where, How: how}
 	attr := &CREATE3res{}
-	res := nfs.Create(args, attr)
+	res := suite.nfs.Create(args, attr)
 	suite.Require().Nil(res)
 	suite.Equal(attr.Status, NFS3_OK)
 }
 
 func (suite *NfsSuite) Lookup(name string) Nfs_fh3 {
-	nfs := suite.nfs
 	what := Diropargs3{Dir: MkRootFh3(), Name: Filename3(name)}
 	args := &LOOKUP3args{What: what}
 	reply := &LOOKUP3res{}
-	res := nfs.Lookup(args, reply)
+	res := suite.nfs.Lookup(args, reply)
 	suite.Require().Nil(res)
 	suite.Equal(reply.Status, NFS3_OK)
 	return reply.Resok.Object
 }
 
-func (suite *NfsSuite) Getattr(fh Nfs_fh3) {
-	nfs := suite.nfs
+func (suite *NfsSuite) Getattr(fh Nfs_fh3, sz uint64) {
 	args := &GETATTR3args{Object: fh}
 	attr := &GETATTR3res{}
-	res := nfs.GetAttr(args, attr)
+	res := suite.nfs.GetAttr(args, attr)
 	suite.Require().Nil(res)
 	suite.Equal(attr.Status, NFS3_OK)
 	suite.Equal(attr.Resok.Obj_attributes.Ftype, NF3REG)
+	suite.Equal(attr.Resok.Obj_attributes.Size, Size3(sz))
 }
 
 func (suite *NfsSuite) Setattr(fh Nfs_fh3, sz uint64) {
-	nfs := suite.nfs
 	size := Set_size3{Set_it: true, Size: Size3(sz)}
 	attr := Sattr3{Size: size}
 	args := &SETATTR3args{Object: fh, New_attributes: attr}
 	reply := &SETATTR3res{}
-	res := nfs.SetAttr(args, reply)
+	res := suite.nfs.SetAttr(args, reply)
 	suite.Require().Nil(res)
 	suite.Equal(reply.Status, NFS3_OK)
 }
 
 func (suite *NfsSuite) TestMakeFile() {
-	nfs := suite.nfs
 	suite.Create("x")
 	fh := suite.Lookup("x")
-	suite.Getattr(fh)
+	suite.Getattr(fh, 0)
 	suite.Setattr(fh, 8192)
-	nfs.ShutdownNfs()
+	suite.Getattr(fh, 8192)
+
+	suite.nfs.ShutdownNfs()
 }
 
 func TestNfs(t *testing.T) {
