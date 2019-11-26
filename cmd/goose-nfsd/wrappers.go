fd1b76460f6a51f0961326d4fbdac2df5272def7
Frans Kaashoek <kaashoek@mit.edu>
2019-11-26 17:18:18 -0500

    Add stubs for remaining supported ops

--- cmd/goose-nfsd/wrappers.go
+++ cmd/goose-nfsd/wrappers.go
@@ -76,6 +76,32 @@ func (w *nfsWrapper) getattr(args *xdr.XdrState) (res xdr.Xdrable, err error) {
 	return &out, err
 }
 
+func (w *nfsWrapper) setattr(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.SETATTR3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.SETATTR3res
+	err = w.nfs.SetAttr(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) lookup(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.LOOKUP3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.LOOKUP3res
+	err = w.nfs.Lookup(&in, &out)
+	return &out, err
+}
+
 func (w *nfsWrapper) access(args *xdr.XdrState) (res xdr.Xdrable, err error) {
 	var in goose_nfs.ACCESS3args
 	in.Xdr(args)
@@ -86,6 +112,71 @@ func (w *nfsWrapper) access(args *xdr.XdrState) (res xdr.Xdrable, err error) {
 
 	var out goose_nfs.ACCESS3res
 	err = w.nfs.Access(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) read(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.READ3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.READ3res
+	err = w.nfs.Read(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) write(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.WRITE3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.WRITE3res
+	err = w.nfs.Write(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) create(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.CREATE3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.CREATE3res
+	err = w.nfs.Create(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) remove(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.REMOVE3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.REMOVE3res
+	err = w.nfs.Remove(&in, &out)
+	return &out, err
+}
+
+func (w *nfsWrapper) rename(args *xdr.XdrState) (res xdr.Xdrable, err error) {
+	var in goose_nfs.RENAME3args
+	in.Xdr(args)
+	err = args.Error()
+	if err != nil {
+		return
+	}
+
+	var out goose_nfs.RENAME3res
+	err = w.nfs.Rename(&in, &out)
 	return &out, err
 }
 
@@ -105,6 +196,8 @@ func (w *nfsWrapper) fsinfo(args *xdr.XdrState) (res xdr.Xdrable, err error) {
 func registerNFS(srv *rfc1057.Server, nfs *goose_nfs.Nfs) {
 	w := &nfsWrapper{nfs}
 
+	// MOUNT
+
 	srv.Register(goose_nfs.MOUNT_PROGRAM, goose_nfs.MOUNT_V3,
 		goose_nfs.MOUNTPROC3_NULL, w.nullmount)
 
@@ -114,6 +207,8 @@ func registerNFS(srv *rfc1057.Server, nfs *goose_nfs.Nfs) {
 	srv.Register(goose_nfs.MOUNT_PROGRAM, goose_nfs.MOUNT_V3,
 		goose_nfs.MOUNTPROC3_EXPORT, w.export)
 
+	// NFS
+
 	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
 		goose_nfs.NFSPROC3_NULL, w.nullnfs)
 
@@ -121,7 +216,31 @@ func registerNFS(srv *rfc1057.Server, nfs *goose_nfs.Nfs) {
 		goose_nfs.NFSPROC3_GETATTR, w.getattr)
 
 	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_SETATTR, w.setattr)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_LOOKUP, w.lookup)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
 		goose_nfs.NFSPROC3_ACCESS, w.access)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_READ, w.read)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_WRITE, w.write)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_CREATE, w.create)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_REMOVE, w.remove)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_REMOVE, w.remove)
+
+	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
+		goose_nfs.NFSPROC3_RENAME, w.rename)
 
 	srv.Register(goose_nfs.NFS_PROGRAM, goose_nfs.NFS_V3,
 		goose_nfs.NFSPROC3_FSINFO, w.fsinfo)
