7b2c3518418b207e9a8fddfdcf96fc3da7a3920e
Frans Kaashoek <kaashoek@mit.edu>
2020-01-23 16:43:50 -0500

    x

---
+++ cmd/fs-smallfile/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func smallfile(name string, data []byte) {
+	f, err := os.Create(name)
+	if err != nil {
+		panic(err)
+	}
+	_, err = f.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+	err = os.Remove(name)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mkdata(sz uint64) []byte {
+	data := make([]byte, sz)
+	for i := range data {
+		data[i] = byte(i % 128)
+	}
+	return data
+}
+
+func main() {
+	const N = 1000 * 1000 * 10
+	path := "/mnt/nfs/x"
+
+	start := time.Now()
+	i := 0
+	data := mkdata(uint64(100))
+	for {
+		s := strconv.Itoa(i)
+		smallfile(path+s, data)
+		i++
+		t := time.Now()
+		elapsed := t.Sub(start)
+		if elapsed.Microseconds() >= N {
+			break
+		}
+	}
+	fmt.Printf("fs-smallfile: %v file/sec\n", float64(i)/(N/(1000*1000)))
+}
