942a7cd490f3717cbbef04849796f530823142f5
Frans Kaashoek <kaashoek@mit.edu>
2020-01-24 11:00:46 -0500

    Simplify buf install and allocator
    Buf install can only be 1 bit or a number of bytes, byte aligned (e.g., inode)
    Allocator can lock only 1 bit
    
    Linux NFS client breaks a large write into 4KB Write RPCs (issued in parallel)

--- run-goose-nfs.sh
+++ run-goose-nfs.sh
@@ -5,10 +5,17 @@
 #
 
 # taskset 0xc go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img &
+
+# go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img -cpuprofile=nfsd.prof &
+
 go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img &
-sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs 
+sleep 1
+sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
+
 # taskset 0x3 $1 /mnt/nfs
 echo "$@"
 "$@"
+
+killall -s SIGINT goose-nfsd
 sudo umount -f /mnt/nfs
-killall goose-nfsd
+
