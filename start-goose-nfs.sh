812189b05e69dc932d738b624c54b6269ac8a4d1
Frans Kaashoek <kaashoek@mit.edu>
2020-01-29 07:53:22 -0500

    start/stop nfs server

---
+++ start-goose-nfs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#
+# Usage:  ./start-goose-nfs.sh
+#
+
+go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img > nfs.out 2>&1 &
+sleep 2
+sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
---
+++ stop-goose-nfs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#
+# Usage:  ./stop-goose-nfs.sh
+#
+
+killall -s SIGINT goose-nfsd
+sudo umount -f /mnt/nfs
