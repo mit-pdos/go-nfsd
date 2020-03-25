17433ad2eb10029f5f5aec0e19eaf10485d63bcb
Lily Tsai <tslilyai@mit.edu>
2020-03-25 12:29:56 -0400

    Add kvs implementation with multiput and get, and simple functionality test

---
+++ kvs/kvs.go
@@ -0,0 +1,78 @@
+package kvs
+
+import (
+	"fmt"
+
+	"github.com/tchajed/goose/machine/disk"
+
+	"github.com/mit-pdos/goose-nfsd/addr"
+	"github.com/mit-pdos/goose-nfsd/buf"
+	"github.com/mit-pdos/goose-nfsd/super"
+	"github.com/mit-pdos/goose-nfsd/txn"
+)
+
+//
+// KVS using txns to implement multiput / multiget transactions
+// Keys == Block addresses
+//
+
+const DISKSZ uint64 = 10 * 1000
+const DISKNAME string = "goose_kvs.img"
+
+type KVS struct {
+	super       *super.FsSuper
+	txn         *txn.Txn
+	presentBufs *buf.BufMap
+}
+
+type KVPair struct {
+	Key addr.Addr
+	Val []byte
+}
+
+func MkKVS() *KVS {
+	d, err := disk.NewFileDisk(DISKNAME, DISKSZ)
+	if err != nil {
+		panic(fmt.Errorf("could not create file disk: %v", err))
+	}
+
+	fsSuper := super.MkFsSuper(d)
+	kvs := &KVS{
+		presentBufs: buf.MkBufMap(),
+		super:       fsSuper,
+		txn:         txn.MkTxn(fsSuper),
+	}
+	return kvs
+}
+
+func (kvs *KVS) MultiPut(pairs []KVPair) bool {
+	var bufs []*buf.Buf
+	for _, p := range pairs {
+		b := kvs.presentBufs.Lookup(p.Key)
+		if b == nil {
+			b = kvs.txn.Load(p.Key)
+			kvs.presentBufs.Insert(b)
+		}
+		if uint64(len(p.Val)*8) != b.Addr.Sz {
+			panic("overwrite")
+		}
+		b.Blk = p.Val
+		bufs = append(bufs, b)
+	}
+	ok := kvs.txn.CommitWait(bufs, true, kvs.txn.GetTransId())
+	return ok
+}
+
+func (kvs *KVS) Get(key addr.Addr) *KVPair {
+	// only return a key if it has been added to the map
+	// otherwise return nil
+	var data []byte
+	b := kvs.presentBufs.Lookup(key)
+	if b != nil {
+		data = b.Blk
+	}
+	return &KVPair{
+		Key: key,
+		Val: data,
+	}
+}
---
+++ kvs/kvs_test.go
@@ -0,0 +1,50 @@
+package kvs
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/mit-pdos/goose-nfsd/addr"
+)
+
+const OBJSZ uint64 = 128
+
+func mkdataval(b byte, sz uint64) []byte {
+	data := make([]byte, sz)
+	for i := range data {
+		data[i] = b
+	}
+	return data
+}
+
+func TestGetAndPuts(t *testing.T) {
+	fmt.Printf("TestGetAndPuts\n")
+	kvs := MkKVS()
+
+	pairs := []KVPair{}
+	addrs := []addr.Addr{}
+	vals := [][]byte{}
+	for i := 0; i < 10; i++ {
+		addrs = append(addrs, addr.MkAddr(uint64(i), 0, OBJSZ*8))
+		vals = append(vals, mkdataval(byte(i), OBJSZ))
+		pairs = append(pairs, KVPair{addrs[i], vals[i]})
+	}
+
+	ok := kvs.MultiPut(pairs)
+	if !ok {
+		log.Fatalf("Puts failed")
+	}
+
+	// ensure that get of a non-present key fails
+	addrs = append(addrs, addr.MkAddr(10, 0, OBJSZ*8))
+	vals = append(vals, []byte{})
+	for i := 0; i < 11; i++ {
+		p := kvs.Get(addrs[i])
+		for j := range p.Val {
+			if p.Val[j] != vals[i][j] {
+				log.Fatalf("Got %d, expected %d", p.Val, vals[i])
+			}
+		}
+	}
+}
