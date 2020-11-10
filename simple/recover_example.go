f210e2950368d54697e8fc5674ac2535281a8778
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-11-10 10:22:28 -0500

    example top-level driver with recovery
    doesn't goose because goose doesn't support passing arguments at
    goroutine creation time..

---
+++ simple/recover_example.go
@@ -0,0 +1,32 @@
+package simple
+
+import (
+	"github.com/tchajed/goose/machine/disk"
+
+	"github.com/mit-pdos/goose-nfsd/common"
+	"github.com/mit-pdos/goose-nfsd/lockmap"
+	"github.com/mit-pdos/goose-nfsd/nfstypes"
+	"github.com/mit-pdos/goose-nfsd/txn"
+)
+
+func exampleWorker(nfs *Nfs, ino common.Inum) {
+	fh := Fh{Ino: ino}
+	buf := make([]byte, 1024)
+	nfs.NFSPROC3_GETATTR(nfstypes.GETATTR3args{Object: fh.MakeFh3()})
+	nfs.NFSPROC3_READ(nfstypes.READ3args{File: fh.MakeFh3(), Offset: 0, Count: 1024})
+	nfs.NFSPROC3_WRITE(nfstypes.WRITE3args{File: fh.MakeFh3(), Offset: 0, Count: 1024, Data: buf})
+}
+
+func RecoverExample(d disk.Disk) {
+	txn := txn.MkTxn(d)
+	lockmap := lockmap.MkLockMap()
+
+	nfs := &Nfs{
+		t: txn,
+		l: lockmap,
+	}
+
+	go exampleWorker(nfs, 3)
+	go exampleWorker(nfs, 3)
+	go exampleWorker(nfs, 4)
+}
