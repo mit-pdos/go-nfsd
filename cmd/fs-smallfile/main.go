043c92eb8071ef925e802cb0e9af0e31573ad8a6
Frans Kaashoek <kaashoek@mit.edu>
2020-11-20 08:42:24 -0500

    Parallel smallfile

--- cmd/fs-smallfile/main.go
+++ cmd/fs-smallfile/main.go
@@ -7,6 +7,11 @@ import (
 	"time"
 )
 
+const (
+	N       = 10 * time.Second
+	NTHREAD = 4
+)
+
 func smallfile(name string, data []byte) {
 	f, err := os.Create(name)
 	if err != nil {
@@ -31,16 +36,13 @@ func mkdata(sz uint64) []byte {
 	return data
 }
 
-func main() {
-	const N = 10 * time.Second
-	path := "/mnt/nfs/x"
-
+func client(p string) int {
+	data := mkdata(uint64(100))
 	start := time.Now()
 	i := 0
-	data := mkdata(uint64(100))
 	for {
 		s := strconv.Itoa(i)
-		smallfile(path+s, data)
+		smallfile(p+s, data)
 		i++
 		t := time.Now()
 		elapsed := t.Sub(start)
@@ -48,5 +50,32 @@ func main() {
 			break
 		}
 	}
-	fmt.Printf("fs-smallfile: %v file/sec\n", float64(i)/N.Seconds())
+	return i
+}
+
+func run(nt int) {
+	path := "/mnt/nfs/"
+	count := make(chan int)
+	for i := 0; i < nt; i++ {
+		d := "d" + strconv.Itoa(i)
+		go func(d string) {
+			err := os.MkdirAll(path+"/"+d+"/", 0700)
+			if err != nil {
+				panic(err)
+			}
+			count <- client(path + d)
+		}(d)
+
+	}
+	n := 0
+	for i := 0; i < nt; i++ {
+		n += <-count
+	}
+	fmt.Printf("fs-smallfile: %v %v file/sec\n", nt, float64(n)/N.Seconds())
+}
+
+func main() {
+	for i := 1; i <= NTHREAD; i++ {
+		run(i)
+	}
 }
