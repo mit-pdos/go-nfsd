89e87abd3732e477a796edab429cce867f4bf8c1
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-12-02 14:59:23 -0500

    goose workaround

--- simple/recover_example.go
+++ simple/recover_example.go
@@ -15,6 +15,7 @@ func exampleWorker(nfs *Nfs, ino common.Inum) {
 	nfs.NFSPROC3_GETATTR(nfstypes.GETATTR3args{Object: fh.MakeFh3()})
 	nfs.NFSPROC3_READ(nfstypes.READ3args{File: fh.MakeFh3(), Offset: 0, Count: 1024})
 	nfs.NFSPROC3_WRITE(nfstypes.WRITE3args{File: fh.MakeFh3(), Offset: 0, Count: 1024, Data: buf})
+	return
 }
 
 func RecoverExample(d disk.Disk) {
