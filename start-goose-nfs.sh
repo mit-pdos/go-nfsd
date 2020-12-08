cc35f2b7a550cf285356bda90c9ce89bb4469a51
Frans Kaashoek <kaashoek@mit.edu>
2020-12-07 19:32:43 -0500

    Revert "Update results, this time the benchmarks makes RPCs to the server"
    This reverts commit e6f07b152f082d463abfc895af772e650ea6219d.

--- start-goose-nfs.sh
+++ start-goose-nfs.sh
@@ -4,11 +4,7 @@
 # Usage:  ./start-goose-nfs.sh
 #
 
-dd if=/dev/zero of=~/tmp/goose.img bs=4K count=100000
-go run ./cmd/goose-nfsd/ -disk ~/tmp/goose.img > nfs.out 2>&1 &
-
-#go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img > nfs.out 2>&1 &
-
+#go run ./cmd/goose-nfsd/ -disk ~/tmp/goose.img > nfs.out 2>&1 &
+go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img > nfs.out 2>&1 &
 sleep 2
-
-#sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
+sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
