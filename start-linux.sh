78169dc703c9425728e1e5f5a81c2d53c4e3e0e7
Frans Kaashoek <kaashoek@mit.edu>
2020-12-10 08:44:49 -0500

    Drop -o sync

--- start-linux.sh
+++ start-linux.sh
@@ -9,7 +9,7 @@ IMG=/tmp/nfs3.img
 rm -f $IMG
 dd if=/dev/zero of=$IMG bs=4K count=100000
 mkfs -t ext3 $IMG
-sudo mount -t ext3 -o data=journal,sync -o loop $IMG /srv/nfs/bench
+sudo mount -t ext3 -o data=journal -o loop $IMG /srv/nfs/bench
 sudo systemctl start nfs-server.service
 sudo mount -t nfs -o vers=3 localhost:/srv/nfs/bench /mnt/nfs
 sudo chmod 777 /srv/nfs/bench
