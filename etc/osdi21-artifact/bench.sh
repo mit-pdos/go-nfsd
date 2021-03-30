e6a10fa1d5f50146427fbd0f612c4ea7a9fd4679
Tej Chajed <tchajed@mit.edu>
2021-03-30 15:20:41 -0400

    Add app bench to bench.sh

--- etc/osdi21-artifact/bench.sh
+++ etc/osdi21-artifact/bench.sh
@@ -4,10 +4,21 @@
 
 set -eo
 
+if [ -d "$GOOSE_NFSD_PATH" ]; then
+    echo "GOOSE_NFSD_PATH is unset" 1>&2
+    exit 1
+fi
+if [ -d "$XV6_PATH" ]; then
+    echo "XV6_PATH is unset" 1>&2
+    exit 1
+fi
+
 cd "$GOOSE_NFSD_PATH"
 
 ./run-goose-nfs.sh go run ./cmd/fs-smallfile -start=10 -threads=10
 ./run-goose-nfs.sh go run ./cmd/fs-largefile
+./run-goose-nfs.sh ./app-bench.sh "$XV6_PATH" /mnt/nfs
 
 ./run-linux.sh go run ./cmd/fs-smallfile -start=10 -threads=10
 ./run-linux.sh go run ./cmd/fs-largefile
+./run-linux.sh ./app-bench.sh "$XV6_PATH" /mnt/nfs
