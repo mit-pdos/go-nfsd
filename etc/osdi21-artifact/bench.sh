3b779d31c5769d048f6e634057773316d294dd98
Tej Chajed <tchajed@mit.edu>
2021-03-30 18:45:32 -0400

    Add script to run scalability numbers

--- etc/osdi21-artifact/bench.sh
+++ etc/osdi21-artifact/bench.sh
@@ -2,7 +2,14 @@
 
 # run various performance benchmarks
 
-set -e
+set -eu
+
+blue=$(tput setaf 4)
+reset=$(tput sgr0)
+
+info() {
+  echo -e "${blue}$1${reset}"
+}
 
 if [ ! -d "$GOOSE_NFSD_PATH" ]; then
     echo "GOOSE_NFSD_PATH is unset" 1>&2
@@ -15,10 +22,13 @@ fi
 
 cd "$GOOSE_NFSD_PATH"
 
+info "GoNFS"
 ./run-goose-nfs.sh go run ./cmd/fs-smallfile -start=10 -threads=10
 ./run-goose-nfs.sh go run ./cmd/fs-largefile
 ./run-goose-nfs.sh ./app-bench.sh "$XV6_PATH" /mnt/nfs
 
+echo
+info "Linux ext3 over NFS"
 ./run-linux.sh go run ./cmd/fs-smallfile -start=10 -threads=10
 ./run-linux.sh go run ./cmd/fs-largefile
 ./run-linux.sh ./app-bench.sh "$XV6_PATH" /mnt/nfs
---
+++ etc/osdi21-artifact/scale.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu
+
+blue=$(tput setaf 4)
+reset=$(tput sgr0)
+
+info() {
+  echo -e "${blue}$1${reset}"
+}
+
+if [ ! -d "$GOOSE_NFSD_PATH" ]; then
+    echo "GOOSE_NFSD_PATH is unset" 1>&2
+    exit 1
+fi
+cd "$GOOSE_NFSD_PATH"
+
+info "GoNFS smallfile scalability"
+./run-goose-nfs.sh -disk ~/disk.img go run ./cmd/fs-smallfile -threads=10
+
+echo
+info "Linux smallfile scalability"
+./run-linux.sh     -disk ~/disk.img go run ./cmd/fs-smallfile -threads=10
