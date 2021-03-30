559e59d063c8d7f317c2ab0e89c93218d5c2ca96
Tej Chajed <tchajed@mit.edu>
2021-03-30 18:34:26 -0400

    Make start-goose-nfs.sh more robust to errors

--- start-goose-nfs.sh
+++ start-goose-nfs.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu
 
 #
 # Usage:  ./start-goose-nfs.sh <arguments>
@@ -9,4 +11,5 @@
 #go run ./cmd/goose-nfsd/ -disk ~/tmp/goose.img > nfs.out 2>&1 &
 go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img "$@" > nfs.out 2>&1 &
 sleep 2
+killall -0 goose-nfsd # make sure server is running
 sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
