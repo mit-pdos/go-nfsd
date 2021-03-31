cbb6a3ba27be51568d07fdc654a872b3b4108fd2
Tej Chajed <tchajed@mit.edu>
2021-03-31 16:27:41 -0400

    Build goose-nfsd separately
    Avoid failing when server isn't compiled after 2 seconds

--- bench/start-goose-nfs.sh
+++ bench/start-goose-nfs.sh
@@ -12,7 +12,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # root of repo
 cd $DIR/..
 
-#go run ./cmd/goose-nfsd/ -disk ~/tmp/goose.img > nfs.out 2>&1 &
+# make sure code is compiled in case it takes longer than 2s to build
+go build ./cmd/goose-nfsd && rm -f goose-nfsd
 go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img "$@" > nfs.out 2>&1 &
 sleep 2
 killall -0 goose-nfsd # make sure server is running
