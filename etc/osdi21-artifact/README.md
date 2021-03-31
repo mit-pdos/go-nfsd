688722f6f8e477c13d73cababec93dade3c1c0cc
Tej Chajed <tchajed@mit.edu>
2021-03-30 21:56:58 -0400

    Extract data from bench.sh output

---
+++ etc/osdi21-artifact/README.md
@@ -0,0 +1,42 @@
+# JrnlCert artifact
+
+## Set environment variables
+
+```sh
+export PERENNIAL_PATH=$HOME/code/perennial
+export GOOSE_NFSD_PATH=$HOME/code/goose-nfsd
+export MARSHAL_PATH=$HOME/code/marshal
+export XV6_PATH=$HOME/code/xv6-public
+```
+
+## Lines of code table
+
+```sh
+./loc.py
+```
+
+## Gather data
+
+```sh
+bench.sh | tee bench-raw.txt`
+```
+
+takes about a minute
+
+```sh
+./scale.sh 10 | tee scale-raw.txt
+```
+
+takes a few minutes
+
+## Produce graphs
+
+3. Produce graphs
+
+```sh
+bench.py bench-raw.txt > bench.data
+```
+
+TODO: process smallfile
+
+TODO: copy in gnuplot scripts
---
+++ etc/osdi21-artifact/bench.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+import sys
+import re
+
+
+def parse_raw(lines):
+    fs = None
+    data = []
+
+    def get_bench_data(pattern, line):
+        m = re.match(pattern, line)
+        if m:
+            return {
+                "fs": fs,
+                "bench": m.group("bench"),
+                "val": float(m.group("val")),
+            }
+        return None
+
+    for line in lines:
+        m = re.match(r"""fs=(?P<fs>.*)""", line)
+        if m:
+            fs = m.group("fs")
+            continue
+        item = get_bench_data(
+            r"""fs-(?P<bench>smallfile): \d* (?P<val>[0-9.]*) file/sec""", line
+        )
+        if item:
+            data.append(item)
+            continue
+        item = get_bench_data(
+            r"""fs-(?P<bench>largefile):.* throughput (?P<val>[0-9.]*) MB/s""",
+            line,
+        )
+        if item:
+            data.append(item)
+            continue
+        item = get_bench_data(
+            r"""(?P<bench>app)-bench (?P<val>[0-9.]*) app/s""", line
+        )
+        if item:
+            data.append(item)
+            continue
+        print("ignored line: " + line, eof="", file=sys.stderr)
+
+    return pd.DataFrame.from_records(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bench", type=argparse.FileType("r"))
+
+    args = parser.parse_args()
+
+    tidy_df = parse_raw(args.bench)
+    df = tidy_df.pivot_table(index="bench", columns="fs", values="val")
+    print(df.to_csv(sep="\t"))
