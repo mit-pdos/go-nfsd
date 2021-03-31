7e0f4ca563a97ad9179ff2c7ef290f5c4cd9e183
Tej Chajed <tchajed@mit.edu>
2021-03-31 12:47:25 -0400

    Add an untested vm setup script

---
+++ etc/osdi21-artifact/vm-setup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd
+
+sudo apt-get update
+sudo apt-get install -y git python3-pip wget
+# select US/Eastern time zone
+echo -e "12\n5" | sudo apt-get install -y tzdata
+sudo apt-get install -y gnuplot-nox
+
+# Get code
+
+git clone https://github.com/mit-pdos/goose-nfsd
+
+mkdir ~/code
+cd ~/code
+# TODO: use specific commits
+git clone https://github.com/mit-pdos/perennial
+git clone https://github.com/mit-pdos/xv6-public
+git clone https://github.com/tchajed/marshal
+cd
+
+# Set up NFS
+
+sudo apt-get install -y rpcbind nfs-common nfs-server
+sudo mkdir -p /srv/nfs/bench
+sudo chown $USER:$USER /srv/nfs/bench
+sudo mkdir -p /mnt/nfs
+sudo chown $USER:$USER /mnt/nfs
+echo "/srv/nfs/bench localhost(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+
+sudo systemctl enable rpcbind
+sudo systemctl enable rpc-statd
+
+# Install Python dependencies
+
+pip3 install argparse pandas
+
+# Install Go and Go dependencies
+
+GO_FILE=go1.16.2.linux-amd64.tar.gz
+wget https://golang.org/dl/$GO_FILE
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $GO_FILE
+rm $GO_FILE
+echo 'export PATH=$HOME/go/bin:/usr/local/go/bin:$PATH' >> ~/.profile
+
+go install github.com/tchajed/goose/cmd/goose@latest
+
+cat >> ~/.profile <<EOF
+export GOOSE_NFSD_PATH=$HOME/goose-nfsd
+export PERENNIAL_PATH=$HOME/code/perennial
+export MARSHAL_PATH=$HOME/code/marshal
+export XV6_PATH=$HOME/code/xv6-public
+EOF
