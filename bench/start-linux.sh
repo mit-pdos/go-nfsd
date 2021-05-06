a21fc54031877ba1541df73ed695a9573190543a
Tej Chajed <tchajed@mit.edu>
2021-05-06 11:11:57 -0400

    Make it easier to customize ext4 mount options

--- bench/start-linux.sh
+++ bench/start-linux.sh
@@ -3,12 +3,14 @@
 set -e
 
 #
-# Usage: ./start-linux.sh <disk file>
+# Usage: ./start-linux.sh <disk file> [ext4_opts]
 #
 # set to /dev/shm/nfs3.img to use tmpfs, or a file to use the disk (through the host
 # file system), or a block device to use a partition directly (NOTE: it will be
 # overwritten; don't run as root)
 #
+# ext4_opts defaults to data=journal if not passed (use data=ordered for the
+# default mode where metadata is journaled but not data)
 
 # Requires /srv/nfs/bench to be set up for NFS export, otherwise you will get
 #
@@ -23,11 +25,17 @@ set -e
 #
 
 disk_file="$1"
+ext4_opts="$2"
+
+if [ -z "$ext4_opts" ]; then
+  ext4_opts="data=journal"
+fi
+
 rm -f "$disk_file"
 dd status=none if=/dev/zero of="$disk_file" bs=4K count=100000
 mkfs.ext4 -q "$disk_file"
 sync "$disk_file"
-sudo mount -t ext4 -o data=journal -o loop "$disk_file" /srv/nfs/bench
+sudo mount -t ext4 -o "$ext4_opts" -o loop "$disk_file" /srv/nfs/bench
 sudo systemctl start nfs-server.service
 sudo mount -t nfs -o vers=3 localhost:/srv/nfs/bench /mnt/nfs
 sudo chmod 777 /srv/nfs/bench
