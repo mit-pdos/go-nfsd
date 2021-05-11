039ec43d048332b2055f87f563bf5e7cfbdf553c
Tej Chajed <tchajed@mit.edu>
2021-05-10 22:42:15 -0400

    Fix run-linux.sh with no arguments

--- bench/run-linux.sh
+++ bench/run-linux.sh
@@ -11,7 +11,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
 disk_file=/dev/shm/nfs3.img
-if [ "$1" = "-disk" ]; then
+if [[ "$#" -gt 0 ]] && [[ "$1" = "-disk" ]]; then
     disk_file="$2"
     shift
     shift
