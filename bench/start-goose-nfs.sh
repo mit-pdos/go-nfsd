7e2e98ac207dc70cb2ca6a96f48125f7c93cb4f2
Tej Chajed <tchajed@mit.edu>
2021-05-14 06:58:59 -0400

    Run with go build rather than go run
    This fixes viewing source in a profile

--- bench/start-goose-nfs.sh
+++ bench/start-goose-nfs.sh
@@ -13,8 +13,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
 # make sure code is compiled in case it takes longer than 2s to build
-go build ./cmd/goose-nfsd && rm -f goose-nfsd
-go run ./cmd/goose-nfsd/ -disk /dev/shm/goose.img "$@" > nfs.out 2>&1 &
+go build ./cmd/goose-nfsd
+./goose-nfsd -disk /dev/shm/goose.img "$@" > nfs.out 2>&1 &
 sleep 2
 killall -0 goose-nfsd # make sure server is running
 sudo mount -t nfs -o vers=3 localhost:/ /mnt/nfs
