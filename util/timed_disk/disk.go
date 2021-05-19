8962febc2ef79c9033d5cfd690018eb0ccf9d344
Tej Chajed <tchajed@mit.edu>
2021-05-18 22:53:46 -0400

    Upgrade to latest goose

--- util/timed_disk/disk.go
+++ util/timed_disk/disk.go
@@ -28,9 +28,15 @@ var ops = []string{"disk.Read", "disk.Write", "disk.Barrier"}
 // assert that Disk implements disk.Disk
 var _ disk.Disk = &Disk{}
 
-func (d *Disk) Read(a uint64) disk.Block {
+func (d *Disk) ReadTo(a uint64, b disk.Block) {
 	defer d.ops[readOp].Record(time.Now())
-	return d.d.Read(a)
+	d.d.ReadTo(a, b)
+}
+
+func (d *Disk) Read(a uint64) disk.Block {
+	buf := make(disk.Block, disk.BlockSize)
+	d.ReadTo(a, buf)
+	return buf
 }
 
 func (d *Disk) Write(a uint64, b disk.Block) {
