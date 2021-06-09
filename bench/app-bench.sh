54267de8f788d5e6f9f681f198848cc618bb9580
Tej Chajed <tchajed@mit.edu>
2021-06-09 10:44:41 -0400

    Use tmpfs for app-bench

--- bench/app-bench.sh
+++ bench/app-bench.sh
@@ -7,7 +7,7 @@
 # Clones xv6-repo to path-to-fs/xv6, then compiles the kernel.
 #
 
-set -e
+set -eu
 
 if [ $# -ne 2 ]; then
     echo "$0 xv6-repo top-dir"
@@ -19,10 +19,15 @@ fs_dir="$2"
 echo "=== app-bench $xv6_repo $fs_dir ===" 1>&2
 cd "$fs_dir"
 
+# run benchmark out of /tmp to avoid overhead of reading source repo
+xv6_tmp="/tmp/xv6"
+rm -rf "$xv6_tmp"
+cp -r "$xv6_repo" "$xv6_tmp"
+
 time_file="/tmp/time"
 
 #echo "=== git clone ==="
-/usr/bin/time -f "clone real %e" -o "$time_file" git clone --quiet "$xv6_repo" xv6
+/usr/bin/time -f "clone real %e" -o "$time_file" git clone --quiet "$xv6_tmp" xv6
 clone_time="$(cut -d ' ' -f3 <"$time_file")"
 cat "$time_file" 1>&2
 
@@ -37,3 +42,5 @@ total_time=$(awk "BEGIN{ print $clone_time + $compile_time }")
 throughput=$(awk "BEGIN{ print 1.0 / $total_time }")
 echo "total real $total_time s" 1>&2
 echo "app-bench $throughput app/s"
+
+rm -r "$xv6_tmp"
